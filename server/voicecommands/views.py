7d94ab53b7a9ce97b935d68edf50e0d85a98082f
redmac135 <ethanzhao1981@gmail.com>
2024-04-08 21:44:49 -0400

    implement voice code into voicecommands app

--- server/voicecommands/views.py
+++ server/voicecommands/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import VoiceCommand
+from contacts.models import Contact
 
 # Create your views here.
+
+
+class VoiceCommandView(APIView):
+    model = VoiceCommand
+
+    def post(self, request):
+        audio = request.data["audio"]
+        # convert audio to text
+        text = "create contact bob miller"
+        command_pk, name = VoiceCommand.parse_command(text)
+
+        if command_pk == 1:
+            Contact.create_contact_by_name(name)
+            return Response({"message": "Contact Created"}, status=200)
+        elif command_pk == 2:
+            contactObj = Contact.get_contact_by_name(name)
+            if contactObj == None:
+                return Response({"message": "Contact not found"}, status=404)
+            return Response(
+                {"message": "Contact found", "contact_pk": str(contactObj.pk)},
+                status=200,
+            )
+        else:
+            return Response({"message": "Command not Recognized"}, status=404)
