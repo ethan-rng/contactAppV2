a483050129088a5b679f89ee15ba8f72d5672734
redmac135 <ethanzhao1981@gmail.com>
2024-04-09 01:36:19 -0400

    implement voicecommand urls

---
+++ server/voicecommands/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import VoiceCommandView, TextCommandView
+
+urlpatterns = [
+    path("voicecommand/", VoiceCommandView.as_view()),
+    path("textcommand/", TextCommandView.as_view()),
+]
